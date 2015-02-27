5019157556c635108fa4bbf2b0301a19dac9b2ed
Tenurian <tfeuerbo@gmail.com>
2015-02-27 14:54:29 -0700

    messing with weather gui again
    Signed-off-by: Tenurian <tfeuerbo@gmail.com>

--- src/mainmenu/weather/WeatherTest.java
+++ src/mainmenu/weather/WeatherTest.java
@@ -1,6 +1,9 @@
 package mainmenu.weather;
 
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -17,17 +20,24 @@ public class WeatherTest {
 	public static  ArrayList<Image> images;
 	public static JFrame frame = new JFrame();
 	public static void main(String[] args) throws IOException {
+		frame.setPreferredSize(new Dimension(750,100));
+		frame.setLayout(new GridLayout());
 		WeatherInfo w = new WeatherInfo();
 		Calendar c = Calendar.getInstance();
 		c.setTime(c.getTime());
 		int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
-		frame.setLayout(new BoxLayout(frame, BoxLayout.Y_AXIS));
+//		frame.setLayout(new BoxLayout(frame, BoxLayout.Y_AXIS));
 		for(int i = 0; i < 7; i++){
 			String forecast = "";
 			forecast+= days[dayOfWeek-1+i];
 			forecast+=": \nTemp: "+w.getTemp(i)+"\nConditions: "+w.getDesc(i);
 			System.out.println(forecast);
-			images.add(ImageIO.read(w.getIconURL(i)));
+			System.out.println(w.getIconURL(i));
+	        BufferedImage image = ImageIO.read((java.net.URL)w.getIconURL(i));
+	        JLabel label = new JLabel(new ImageIcon(image));
+	        frame.add(label);
 		}
+		frame.pack();
+		frame.setVisible(true);
 	}
 }
